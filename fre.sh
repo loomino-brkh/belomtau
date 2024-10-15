5ed8f6ba183e9a41d56865b425c97921796b8f14
doci <loomino@brkh.work>
2024-10-15 10:10:11 +0000

    October 15, 2024 at 5:10:11 PM GMT+7

--- fre.sh
+++ fre.sh
@@ -10,24 +10,20 @@ PROJECT_DIR="$HOME/eskrim/fre_${APP_NAME}"
 PYTHON_IMAGE="docker.io/library/python:latest"
 NGINX_IMAGE="docker.io/library/nginx:latest"
 
-POD_NAME="${APP_NAME}_pod"
+POD_NAME="${APP_NAME}_fre_pod"
 
-GUNICORN_CONTAINER_NAME="${APP_NAME}_gunicorn"
-NGINX_CONTAINER_NAME="${APP_NAME}_nginx"
-FRONTEND_CONTAINER_NAME="${APP_NAME}_frontend"
-CFL_TUNNEL_CONTAINER_NAME="${APP_NAME}_cfltunnel"
-INTERACT_CONTAINER_NAME="${APP_NAME}_interact"
+GUNICORN_CONTAINER_NAME="${APP_NAME}_fre_gunicorn"
+FRONTEND_CONTAINER_NAME="${APP_NAME}_fre_frontend"
+CFL_TUNNEL_CONTAINER_NAME="${APP_NAME}_fre_cfltunnel"
+INTERACT_CONTAINER_NAME="${APP_NAME}_fre_interact"
 
 REQUIREMENTS_FILE="${PROJECT_DIR}/requirements.txt"
 SETTINGS_FILE="${PROJECT_DIR}/${APP_NAME}/${APP_NAME}/settings.py"
 
 init() {
     [ ! -d "$PROJECT_DIR" ] && mkdir -p "$PROJECT_DIR"
-    [ ! -d "$PROJECT_DIR/db_data" ] && mkdir -p "$PROJECT_DIR/db_data"
-    [ ! -d "$PROJECT_DIR/redis_data" ] && mkdir -p "$PROJECT_DIR/redis_data"
     [ ! -d "$PROJECT_DIR/.root" ] && mkdir -p "$PROJECT_DIR/.root"
     [ ! -f "$PROJECT_DIR/token" ] && touch "$PROJECT_DIR/token"
-    [ ! -d "$PROJECT_DIR/pgadmin" ] && mkdir -p "$PROJECT_DIR/pgadmin" && chmod 777 "$PROJECT_DIR/pgadmin"
 
     cat >"$REQUIREMENTS_FILE" <<EOL
 Django
@@ -37,63 +33,21 @@ EOL
     podman run --rm -v "$PROJECT_DIR:/app" "$PYTHON_IMAGE" python -m venv /app/venv
     podman run --rm -v "$PROJECT_DIR:/app" -w /app "$PYTHON_IMAGE" bash -c "source /app/venv/bin/activate && pip install --upgrade pip && pip install -r /app/requirements.txt"
     podman run --rm -v "$PROJECT_DIR:/app" -w /app "$PYTHON_IMAGE" bash -c "/app/venv/bin/django-admin startproject $APP_NAME"
-    [ ! -d "$PROJECT_DIR/${APP_NAME}/static" ] && mkdir -p "$PROJECT_DIR/${APP_NAME}/static"
+    [ ! -d "$PROJECT_DIR/${APP_NAME}/staticfiles" ] && mkdir -p "$PROJECT_DIR/${APP_NAME}/staticfiles"
+    [ ! -d "$PROJECT_DIR/${APP_NAME}/staticfiles/css" ] && mkdir -p "$PROJECT_DIR/${APP_NAME}/staticfiles/css"
+    [ ! -d "$PROJECT_DIR/${APP_NAME}/staticfiles/js" ] && mkdir -p "$PROJECT_DIR/${APP_NAME}/staticfiles/js"
 
-    sed -i "s/ALLOWED_HOSTS = \[\]/ALLOWED_HOSTS = \[/g" "${SETTINGS_FILE}"
-    sed -i "/ALLOWED_HOSTS = \[/a\     '$HOST_IP',\n     '$HOST_DOMAIN',\n\]" "${SETTINGS_FILE}"
-    sed -i "s/'ENGINE': 'django.db.backends.sqlite3'/'ENGINE': 'django.db.backends.postgresql'/g" "${SETTINGS_FILE}"
-    sed -i "s/'NAME': BASE_DIR \/ 'db.sqlite3'/'NAME': '$POSTGRES_DB'/g" "${SETTINGS_FILE}"
-    sed -i "/'NAME': '$POSTGRES_DB'/a\        'USER': '$POSTGRES_USER',\n        'PASSWORD': '$POSTGRES_PASSWORD',\n        'HOST': 'localhost',\n        'PORT': '5432'," "${SETTINGS_FILE}"
-
-    cat <<EOL >>"${SETTINGS_FILE}"
-CACHES = {
-    'default': {
-        'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://localhost:6379/1',
-        'OPTIONS': {
-            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
-        }
-    }
-}
-EOL
-
-    podman run --rm --pod "$POD_NAME" --name "$APP_NAME"_migrate -v "$PROJECT_DIR:/app:ro" -w /app/"$APP_NAME" "$PYTHON_IMAGE" bash -c "source /app/venv/bin/activate && python manage.py migrate"
-
-    cat >"$PROJECT_DIR/gunicorn.sh" <<EOL
+    if [ ! -f "$PROJECT_DIR/gunicorn.sh" ] || ! grep -q "exec gunicorn --reload --log-level=debug --workers 5 --bind 0.0.0.0:8900 $APP_NAME.wsgi:application" "$PROJECT_DIR/gunicorn.sh"; then
+        cat >"$PROJECT_DIR/gunicorn.sh" <<EOL
+    fi
 #!/bin/bash
 source /app/venv/bin/activate
 cd /app/${APP_NAME}
-exec gunicorn --reload --log-level=debug --workers 5 --bind 0.0.0.0:8000 $APP_NAME.wsgi:application
+exec gunicorn --reload --log-level=debug --workers 5 --bind 0.0.0.0:8900 $APP_NAME.wsgi:application
 EOL
 
     chmod +x "$PROJECT_DIR/gunicorn.sh"
 
-    cat >"$PROJECT_DIR/nginx.conf" <<EOL
-server {
-    listen $PORT1;
-    server_name 127.0.0.1;
-
-    location / {
-        proxy_pass http://127.0.0.1:8000;
-        proxy_set_header Host \$host;
-        proxy_set_header X-Real-IP \$remote_addr;
-        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto \$scheme;
-    }
-
-    location /static/ {
-        alias /www/staticfiles/;
-    }
-    
-    location /api/ {
-        proxy_pass http://127.0.0.1:8000/api/;
-        proxy_set_header Host \$host;
-        proxy_set_header X-Real-IP \$remote_addr;
-        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto \$scheme;
-    }
-}
-EOL
     cat >"$PROJECT_DIR/frontend.conf" <<EOL
 server {
     listen $PORT2;
@@ -112,32 +66,12 @@ stop() {
     podman pod rm "$POD_NAME"
 }
 
-run_postgres() {
-    podman run -d --pod "$POD_NAME" --name "$POSTGRES_CONTAINER_NAME" \
-        -e POSTGRES_DB="$POSTGRES_DB" \
-        -e POSTGRES_USER="$POSTGRES_USER" \
-        -e POSTGRES_PASSWORD="$POSTGRES_PASSWORD" \
-        -v "$PROJECT_DIR/db_data:/var/lib/postgresql/data:z" \
-        "$POSTGRES_IMAGE"
-}
-
-run_redis() {
-    podman run -d --pod "$POD_NAME" --name "$REDIS_CONTAINER_NAME" \
-        -v "$PROJECT_DIR/redis_data:/data:z" \
-        "$REDIS_IMAGE" --loglevel verbose
-}
-
-run_nginx() {
-    podman run -d --pod "$POD_NAME" --name "$NGINX_CONTAINER_NAME" \
-        -v "$PROJECT_DIR/nginx.conf:/etc/nginx/conf.d/default.conf:ro" \
-        -v "$PROJECT_DIR/${APP_NAME}/staticfiles:/www/staticfiles:ro" \
-        "$NGINX_IMAGE"
-}
 
 run_frontend() {
     podman run -d --pod "$POD_NAME" --name "$FRONTEND_CONTAINER_NAME" \
         -v "$PROJECT_DIR/frontend.conf:/etc/nginx/conf.d/default.conf:ro" \
         -v "$PROJECT_DIR/frontend:/www/frontend:ro" \
+        -v "$PROJECT_DIR/${APP_NAME}/staticfiles:/www/staticfiles:ro" \
         "$NGINX_IMAGE"
 }
 
@@ -161,24 +95,12 @@ run_interact() {
         "$PYTHON_IMAGE" bash -c "sleep infinity"
 }
 
-pg() {
-    podman run -d --rm --pod "$POD_NAME" --name "$PGADMIN_CONTAINER_NAME" \
-        -e "PGADMIN_DEFAULT_EMAIL=dyka@brkh.work" \
-        -e "PGADMIN_DEFAULT_PASSWORD=SuperSecret" \
-        -e "PGADMIN_LISTEN_PORT=5050" \
-        -v "$PROJECT_DIR/pgadmin:/var/lib/pgadmin:z" \
-        "$PGADMIN_IMAGE"
-}
-
 pod_create() {
     podman pod create --name "$POD_NAME" --network bridge
 }
 
 esse() {
-    run_postgres
-    run_redis
     run_gunicorn
-    run_nginx
     run_frontend
     run_cfl_tunnel
     run_interact
@@ -193,7 +115,7 @@ start() {
 cek() {
     if podman pod exists "$POD_NAME"; then
         if [ "$(podman pod ps --filter name="$POD_NAME" --format "{{.Status}}" | awk '{print $1}')" = "Running" ]; then
-            for container in "${POSTGRES_CONTAINER_NAME}" "${REDIS_CONTAINER_NAME}" "${GUNICORN_CONTAINER_NAME}" "${NGINX_CONTAINER_NAME}" "${CFL_TUNNEL_CONTAINER_NAME}" "${FRONTEND_CONTAINER_NAME}" "${INTERACT_CONTAINER_NAME}"; do
+            for container in "${GUNICORN_CONTAINER_NAME}" "${NGINX_CONTAINER_NAME}" "${CFL_TUNNEL_CONTAINER_NAME}" "${FRONTEND_CONTAINER_NAME}" "${INTERACT_CONTAINER_NAME}"; do
                 if [ "$(podman ps --filter name="$container" --format "{{.Status}}" | awk '{print $1}')" != "Up" ]; then
                     echo "Container $container is not running. Restarting..."
                     podman start "$container"
