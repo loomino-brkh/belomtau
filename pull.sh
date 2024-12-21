c8381aa0f173fc2482fbc7acd28e44d5082f6d5f
loomino <loomino@brkh.work>
2024-12-21 18:30:33 +0700

    Sabtu, 21 Desember 2024 06:30:33 PM: Auto-commit: Changes in pull.sh

--- pull.sh
+++ pull.sh
@@ -17,6 +17,6 @@ monitor_and_pull() {
 
 while true; do
   monitor_and_pull
-  echo "Monitoring $DIRECTORY for changes..."
+  echo "[$(date '+%Y-%m-%d %H:%M:%S')] Monitoring $DIRECTORY for changes..."
   sleep 15
 done
