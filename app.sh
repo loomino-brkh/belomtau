066fe34310f46616d3ae17c0588a8e972c663c1a
loomino <loomino@brkh.work>
2025-01-04 06:24:13 +0700

    Sabtu, 04 Januari 2025 06:24:13 AM: Auto-commit: Changes in app.sh

--- app.sh
+++ app.sh
@@ -202,7 +202,8 @@ db() {
   podman run -it --rm --pod "${POD_NAME}" \
     -v "${PROJECT_DIR}:/app:z" \
     -w "/app/${APP_NAME}" \
-    "${PYTHON_IMAGE}" bash -c "source /app/venv/bin/activate && python manage.py makemigrations && echo "waiting..." && sleep 5 && python manage.py migrate --verbosity 3"
+    "${PYTHON_IMAGE}" bash -c \
+    "source /app/venv/bin/activate && python manage.py makemigrations && echo "waiting..." && sleep 5 && python manage.py migrate --verbosity 3"
 }
 
 pod_create() {
